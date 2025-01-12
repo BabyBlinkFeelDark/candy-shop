3c0164e0b4bd0ed5963e16ea32f0dd353b7ff17f
Oleg Lomtev <lomtev.oa@gmail.com>
2025-01-12 16:03:47 +0300

    парсинг

--- src/model.py
+++ src/model.py
@@ -1,3 +1,6 @@
+import json
+
+
 class Employee:  # Название класса
     name: str  # Атрибуты (свойства) класса
     description: str
@@ -24,3 +27,26 @@ class Category:  # Название класса
         self.products = products
         Category.total_categories += 1
         Category.total_products += len(products)
+
+    def parser_json(self, file_path: str):
+        """
+        Парсит JSON-файл по указанному пути и возвращает данные в виде списка словарей.
+
+        Args:
+            file_path (str): Путь к JSON-файлу.
+
+        Returns:
+            list[dict]: Список транзакций, если данные корректны. Пустой список, если файл отсутствует
+                        или данные не являются списком.
+        """
+        try:
+            with open(file_path, "r") as f:
+                data = json.load(f)
+                if not isinstance(data, list):
+                    return []
+                for item in data:
+                    self.name = item["name"]
+                    self.description = item['description']
+                    self.products = item['products']
+        except (FileNotFoundError, json.JSONDecodeError):
+            return []
