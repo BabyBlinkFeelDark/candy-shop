27ad61d1ac837ed73062ab5f4288e30ce2a6d867
Oleg Lomtev <lomtev.oa@gmail.com>
2025-01-12 16:07:30 +0300

    метод не фурычит

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,6 +1,10 @@
+import json
+from io import StringIO
+
 import pytest
 
 from src.model import Employee, Category
+from unittest.mock import Mock, patch
 
 
 @pytest.fixture
@@ -23,3 +27,20 @@ def test_init_count(category_coffe):
     assert category_coffe.products == ["coffe"]
     assert category_coffe.total_categories == 1
     assert category_coffe.total_products == 1
+
+
+def test_parser_json_valid_data():
+    json_data = [
+        {"name": "Category1", "description": "Description1", "products": ["prod1", "prod2"]},
+        {"name": "Category2", "description": "Description2", "products": ["prod3"]}
+    ]
+
+
+    with patch("builtins.open", return_value=StringIO(json.dumps(json_data))):
+        category = Category("", "", [])
+        result = category.parser_json("fake_path.json")
+
+        assert category.name == "Category2"
+        assert category.description == "Description2"
+        assert category.products == ["prod3"]
+        assert result == json_data
