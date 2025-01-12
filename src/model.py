68d0c5171521cd7b21db894009c1608de64f4a6d
Oleg Lomtev <lomtev.oa@gmail.com>
2025-01-12 16:17:40 +0300

    доки

--- src/model.py
+++ src/model.py
@@ -1,28 +1,71 @@
 import json
 
 
-class Product:  # Название класса
-    name: str  # Атрибуты (свойства) класса
+class Product:
+    """
+    Класс для представления продукта.
+
+    Атрибуты:
+        name (str): Название продукта.
+        description (str): Описание продукта.
+        price (float): Цена продукта.
+        quantity (int): Количество товара в наличии.
+
+    Методы:
+        __init__: Инициализирует объект продукта с заданными значениями.
+    """
+    name: str
     description: str
     price: float
     quantity: int
 
-    def __init__(self, name, description, price, quantity):  # Конструктор
-        self.name = name  # Атрибуты (свойства) класса
+    def __init__(self, name, description, price, quantity):
+        """
+        Инициализирует объект продукта с указанными параметрами.
+
+        Args:
+            name (str): Название продукта.
+            description (str): Описание продукта.
+            price (float): Цена продукта.
+            quantity (int): Количество продукта в наличии.
+        """
+        self.name = name
         self.description = description
         self.price = price
         self.quantity = quantity
 
 
-class Category:  # Название класса
-    name: str  # Атрибуты (свойства) класса
+class Category:
+    """
+    Класс для представления категории товаров.
+
+    Атрибуты:
+        name (str): Название категории.
+        description (str): Описание категории.
+        products (list): Список товаров, относящихся к категории.
+        total_categories (int): Общее количество созданных категорий.
+        total_products (int): Общее количество товаров во всех категориях.
+
+    Методы:
+        __init__: Инициализирует объект категории.
+        parser_json: Парсит JSON-файл и обновляет атрибуты объекта с данными.
+    """
+    name: str
     description: str
     products: list
     category_count: int = 0
     product_count: int = 0
 
-    def __init__(self, name, description, products):  # Конструктор
-        self.name = name  # Атрибуты (свойства) класса
+    def __init__(self, name, description, products):
+        """
+        Инициализирует объект категории с заданными значениями.
+
+        Args:
+            name (str): Название категории.
+            description (str): Описание категории.
+            products (list): Список товаров в категории.
+        """
+        self.name = name
         self.description = description
         self.products = products
         Category.category_count += 1
